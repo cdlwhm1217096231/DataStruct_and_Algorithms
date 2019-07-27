82d31ebbfe148adbdb430a4e0a1bcf771e21818d
cdl <1217096231@qq.com>
2019-07-27 12:25:45 +0800

    剑指offer--C++版本

--- 剑指offer练习代码/10.二叉树_重建二叉树.cpp
+++ 剑指offer练习代码/10.二叉树_重建二叉树.cpp
@@ -15,17 +15,17 @@ struct BinaryTreeNode{
 
 class Solution{
     public:
-        BinaryTreeNode *reConstructBinaryTree(vector<int> preoder, vector<int> midorder){
-            if(preoder.size() == 0)
+        BinaryTreeNode *reConstructBinaryTree(vector<int> preorder, vector<int> midorder){
+            if(preorder.size() == 0)
                 return NULL;
             vector<int> left_preorder, right_preorder, left_midorder, right_midorder; 
             // 前序遍历第一节点一定是根节点
-            BinaryTreeNode *head = new BinaryTreeNode(preoder[0]);
+            BinaryTreeNode *head = new BinaryTreeNode(preorder[0]);
             // 找到中序遍历中的根节点位置
             int root = 0;
             // 遍历找到根节点在中序遍历中的索引值
-            for(int i = 0;i<preoder.size();++i){
-                if(preoder[0] == midorder[i]){
+            for(int i = 0;i<preorder.size();++i){
+                if(preorder[0] == midorder[i]){
                     root = i;
                     break;
                 }
@@ -33,11 +33,11 @@ class Solution{
             // 利用中序遍历找到的根节点位置，对二叉树节点进行分割，分割成左右两部分的子树
             for(int i = 0; i < root; ++i){
                 left_midorder.push_back(midorder[i]);  // 中序遍历中的左子树
-                left_preorder.push_back(preoder[i+1]); // 前序遍历中的左子树
+                left_preorder.push_back(preorder[i+1]); // 前序遍历中的左子树
             }
-            for(int i = root+1; i < preoder.size();++i){  
+            for(int i = root+1; i < preorder.size();++i){  
                 right_midorder.push_back(midorder[i]);  // 中序遍历中的右子树
-                right_preorder.push_back(preoder[i]);   // 前序遍历中的右子树
+                right_preorder.push_back(preorder[i]);   // 前序遍历中的右子树
             }
             // 再递归上面的操作
             head->left = reConstructBinaryTree(left_preorder, left_midorder);
